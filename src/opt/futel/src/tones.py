86fa562c3b002d44c0debeb7ea4ac529402701af
jason <jason@noisybox.net>
2021-03-04 21:14:09 -0800

    play audio after ring

--- src/opt/futel/src/tones.py
+++ src/opt/futel/src/tones.py
@@ -22,3 +22,6 @@ class Tones:
         if key in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
             key = int(key)
         self.client.send_message('/key', key)
+
+    def play_audio(self, basename):
+        self.client.send_message('/play', basename)
